e98951a07af61731e675663e91fe887e86172c51
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-11-25 14:08:34 -0800

    Update scripts.js

--- vrController/js/scripts.js
+++ vrController/js/scripts.js
@@ -3,19 +3,17 @@ window.onload = (event) => {
   var y = document.getElementById("y_val");
   var z = document.getElementById("z_val");
 
-  let sensor = new Gyroscope();
+  //let sensor = new Gyroscope();
+  let sensor = new AbsoluteOrientationSensor({frequency: 60});
   sensor.start();
-
-  sensor.onreading = () => {
-      console.log("Angular velocity around the X-axis " + sensor.x);
-      console.log("Angular velocity around the Y-axis " + sensor.y);
-      console.log("Angular velocity around the Z-axis " + sensor.z);
-      x.innerHTML = sensor.x.toFixed(2);
-      y.innerHTML = sensor.y.toFixed(2);
-      z.innerHTML = sensor.z.toFixed(2);
+  
+  sensor.onreading = () => {    
+    sensor.onreading = () => model.quaternion.fromArray(sensor.quaternion);
   };
 
   sensor.onerror = event => console.log(event.error.name, event.error.message);
+  
+
 
 };
 
