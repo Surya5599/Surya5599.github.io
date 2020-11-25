f67e413d0e77d820f7b5a85109ca070215fc1e4b
Surya <ssurya5599@gmail.com>
2020-11-25 13:09:51 -0800

    vr website

---
+++ vrController/js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = (event) => {
+  var x = document.getElementById("x_val");
+  var y = document.getElementById("y_val");
+  var z = document.getElementById("z_val");
+
+  let sensor = new Gyroscope();
+
+};
+
+
+
