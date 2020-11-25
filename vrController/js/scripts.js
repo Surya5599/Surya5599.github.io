69b9a67146fffe05b933c3e7931b6a84210fe4ed
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-11-25 13:17:06 -0800

    Update scripts.js

--- vrController/js/scripts.js
+++ vrController/js/scripts.js
@@ -4,6 +4,15 @@ window.onload = (event) => {
   var z = document.getElementById("z_val");
 
   let sensor = new Gyroscope();
+  sensor.start();
+
+  sensor.onreading = () => {
+      console.log("Angular velocity around the X-axis " + sensor.x);
+      console.log("Angular velocity around the Y-axis " + sensor.y);
+      console.log("Angular velocity around the Z-axis " + sensor.z);
+  };
+
+  sensor.onerror = event => console.log(event.error.name, event.error.message);
 
 };
 
