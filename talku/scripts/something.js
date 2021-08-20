05694f6d849e9881324c267f14890aca08a11869
Surya Singh <ssurya5599@gmail.com>
2021-08-19 23:32:43 -0700

    talku fix

--- talku/scripts/something.js
+++ talku/scripts/something.js
@@ -156,28 +156,28 @@ setInterval(function() {
     
 function getLeaderBoards(){
     var theUrl = "https://dt-apigatewayv2.dt-pn1.com/game/rank/list?appId=67&timeStamp=1629436329843";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-    xmlHttp.send( null );
-    var leaderBoards = JSON.parse(xmlHttp.responseText);
-    var currLeader = leaderBoards.data[0];
-    $('#leaderName').text(currLeader.nickname);
-    $('#leaderScore').text(currLeader.bestScore);
+    fetch(theUrl)
+    .then(response => response.json())
+    .then(data => {
+        var currLeader = data.data[0];
+        $('#leaderName').text(currLeader.nickname);
+        $('#leaderScore').text(currLeader.bestScore);
+    });
     getScores();
 }
 
 function getScores(){
     var theUrl = "https://dt-apigatewayv2.dt-pn1.com/game/challenge/info?uid=14513829137484867&nickname=Raven&timeStamp=1629437620104";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-    xmlHttp.send( null );
-    var leaderBoards = JSON.parse(xmlHttp.responseText);
-    $('#alkaRank').text(leaderBoards.data.wordRank);
+    fetch(theUrl)
+    .then(response => response.json())
+    .then(data => {
+        $('#alkaRank').text(data.data.wordRank);;
+    });
 
     var theUrl = "https://dt-apigatewayv2.dt-pn1.com/game/challenge/info?uid=14513822531255167&nickname=Elijah&timeStamp=1629437620104";
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, true ); // false for synchronous request
-    xmlHttp.send( null );
-    var leaderBoards = JSON.parse(xmlHttp.responseText);
-    $('#ashuRank').text(leaderBoards.data.wordRank);
+    fetch(theUrl)
+    .then(response => response.json())
+    .then(data => {
+        $('#ashuRank').text(data.data.wordRank);;
+    });
 }
