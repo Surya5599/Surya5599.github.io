08be07e6fb8f5edd1c138102274d62eb577b8ea1
Surya <ssurya5599@gmail.com>
2021-11-22 20:41:57 -0800

    fixed spinthewheel

--- spinthewheel/script.js
+++ spinthewheel/script.js
@@ -4,12 +4,12 @@ let number = Math.ceil(Math.random() * 1000);
 let previousNumber = 0;
 
 function spinthewheel() {
-	number = Math.ceil((Math.random() + 1000) * 10000);
+	number = Math.ceil((Math.random() + 1000) * 1000);
 	while(true){
 		if(number % 360 < 20 && Math.abs(previousNumber - number) > 720){
 			break;
 		}
-		number = Math.ceil(Math.random() * 10000);
+		number = Math.ceil((Math.random()  + 1000) * 1000);
 	}
 	container.style.transform = "rotate(" + number + "deg)";
 	previousNumber = number;
